e7f0084c055f9739ce02e4c3dca31f6a81e7b48f
Kevin Locke <kevin@kevinlocke.name>
2019-09-15 14:23:45 -0600

    Use process.exitCode rather than process.exit
    Allowing the process to exit on its own when there is no further code to
    execute helps identify lingering chains of execution and avoids abruptly
    terminating them in places where it is not expected.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- test-bin/after-yaku-ok.js
+++ test-bin/after-yaku-ok.js
@@ -12,14 +12,11 @@ const { read } = require('..');
 
 /* eslint-disable no-console */
 
-let exitCode = 0;
 if (yaku.nextTick !== process.nextTick) {
   console.error('yaku.nextTick !== process.nextTick');
-  exitCode = 1;
+  process.exitCode = 1;
 }
 if (read(new EventEmitter()).constructor.nextTick.name !== 'thisTick') {
   console.error('read().constructor.nextTick.name !== \'thisTick\'');
-  exitCode = 1;
+  process.exitCode = 1;
 }
-// eslint-disable-next-line no-process-exit
-process.exit(exitCode);
